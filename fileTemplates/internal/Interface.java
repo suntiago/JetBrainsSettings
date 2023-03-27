8d2afbffbde94d44fe70be08e4649a3f6390a472
zaiyu <zaiyu@smartlink.com.cn>
2023-03-27 21:35:09 +0800

    AI-Electric Eel | 2022.1.1 Patch 1 <zaiyu@DESKTOP-TR3H2PF Create baseRefactoring.xml, ide.general.xml, git.xml, project.default.xml, rainbow_brackets.xml, diff.xml, Enum.java, getterTemplates.xml, Interface.java, plantuml.xml, gradle.experimental.xml, terminal.xml, Default.xml, visualizationTool.xml, androidStudioFirstRun.xml, find.xml, ui.lnf.xml, DontShowAgainFeedbackService.xml, filetypes.xml, checkstyle-idea.xml, editor-font.xml, AndroidLog.xml, colors.scheme.xml, kotlinRefactoring.xml, gerrit_settings.xml, notifications.xml, _@user_Darcula.icls, AndroidLogKotlin.xml, templates.xml, AnnotationType.java, laf.xml, Default.xml, Class.java, jdk.table.xml, acceptedLanguageLevels.xml, vcs.xml, codota.xml, toStringTemplates.xml, editor.xml, debugger.xml, setterTemplates.xml, updates.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * ${NAME}
+ *
+ * @author ${USER}
+ * @description: 
+ * @date :${DATE} ${TIME}
+ */
+public interface ${NAME} {
+}
